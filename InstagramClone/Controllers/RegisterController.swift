ff6b52c5f0bdc9238677028464e7d99e1a1de67a
btider-salih <salih@btider.gov.tr>
2018-11-21 14:30:48 +0300

    Initial commit

---
+++ InstagramClone/Controllers/RegisterController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterController.swift
+//  InstagramClone
+//
+//  Created by BTK Apple on 21.11.2018.
+//  Copyright © 2018 msalihkocak. All rights reserved.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
